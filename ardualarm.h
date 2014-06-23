0af0d31402a0d8c547eb16a14a348ad990c7ee9b
Fergus Morrow <fm@qinec.com>
2014-06-23 11:45:20 +0000

    Some mild refactoring

--- ardualarm.h
+++ ardualarm.h
@@ -15,6 +15,7 @@
 #define LED_GRE 9
 #define FMQ 13
 #define DHT_SENSOR 8
+#define SMO_SENSOR 0
 
 // - LCD PINS (Hitache HD44780 Compat.)
 #define LCD_ENABLE 11
@@ -26,9 +27,7 @@
 
 // BOUNDS
 #define HUMI_MAX 50
-#define HUMI_MIN
 #define TEMP_MAX 25
-#define TEMP_MIN
 
 /*
 ** MACROS
@@ -45,20 +44,125 @@
 ** TYPE DEFINITIONS
 */
 
+// Container for Sensor values
 typedef struct {
-  int minTemp;
+  // Sensor Hardware specific ranges.
+  int sensMinTemp;
+  int sensMaxTemp;
+  int sensmMinHumi;
+  int sensMaxHumi;
+
+  // Sensor User defined ranges.
   int maxTemp;
-  int minHumi;
   int maxHumi;
 
+  // Sensor Readings
   bool smoke;
-  int temp;
-  int humi;
-  int tol;
+  int temp = 0;
+  int humi = 0;
+  int tol = 0;
 } Sensors;
 
+
+// LCD Output Modes
 typedef enum {
-  ALL =  0,
-  TEMP = 1,
-  HUMI=  2
+  ALL  =  0,  // DISPLAY ALL SETTINGS
+  TEMP =  1,  // DISPLAY TEMPERATURE
+  HUMI =  2   // DISPLAY HUMIDITY
 } LCDMode;
+
+const int lcd_num_count = HUMI - ALL;
+
+// State enumeration - allows us to hold the entirety of our state in an 8bit int.
+enum State {
+  OUT_R_LED = 0x01,   // OUTPUT:  RED LED
+  OUT_G_LED = 0x02,   // OUTPUT:  GREEN LED
+  OUT_BUZZE = 0x04,   // OUTPUT:  BUZZER
+  IN_SMOKE  = 0x08,   // INPUT:   SMOKE SENSOR
+  //  N/A   = 0x10,   // FUTURE:  USER DEFINED MIN TEMPERATURE
+  IN_TEMP_H = 0x20,   // STATUS:  TEMPERATURE TOO HIGH
+  //  N/A   = 0x40,   // FUTURE:  USER DEFINED MIN HUMIDITY
+  IN_HUMI_H = 0x08    // STATUS:  HUMDITIY TOO HIGH
+};
+
+
+/*
+** INPUT METHODS
+*/
+
+
+// Check btnPin for a press, if so - increment associated var,
+//  do bounds checking - lowering if required.
+void keyIncrementer( int btnPin, int lower, int upper, int &cur )
+{
+  if( buttonCheck( btnPin ) )
+  {
+    cur++;
+    if( cur > upper ) cur = lower;
+  }
+}
+
+// Check for a button press on a given PIN.
+bool buttonCheck( int pin )
+{
+  if( analogRead(pin) > 600 )
+  {
+    delay(20);
+    if( analogRead(pin) > 600 )
+    {
+      while( analogRead(pin) > 600 ); // Blocks execution until button is released.
+      return true;
+    }
+  }
+  return false;
+}
+
+
+/*
+** OUTPUT METHODS
+*/
+
+
+/*
+** LCD OUTPUT METHODS.
+*/
+
+
+// Output all current values.
+void lcd_OutputAll()
+{
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print("TEMP:  C");
+  lcd.setCursor(9,0);
+  lcd.print("SMO:");
+  lcd.setCursor(0,1);
+  lcd.print("HUMI:  %");
+  lcd.setCursor(9,1);
+  lcd.setCursor(5,0);
+  lcd.print( ( sensors.temp > 0 ) ? sensors.temp : 'ERR' );
+  lcd.setCursor(5,1);
+  lcd.print( ( sensors.humi > 0 ) ? sensors.humi : 'ERR' );
+  lcd.setCursor(13,0);
+  lcd.print( (sensors.smoke) ? 1 : 0 );
+}
+
+// Output *alarm temperature* only; not current temperature
+//  TODO: Output both.
+void lcd_OutputTemp()
+{
+  lcd.begin(16,2);
+  lcd.print("ALARM TEMP: ");  // Perhaps display the current val as well as the max val?!?
+  lcd.setCursor(11,0);
+  lcd.print(maxTemp);
+}
+
+// Output *alarm humidity* only; not current humidity
+//  TODO: Output both.
+void lcd_OutputHumi()
+{
+  lcd.begin(16,2);
+  lcd.print("ALARM HUMI: ");  // Perhaps display the current val as well as the max val?!?
+  lcd.setCursor(12,0);
+  lcd.print(maxHumi);
+}
--- source.ino
+++ source.ino
@@ -9,248 +9,166 @@
 
 #include <LiquidCrystal.h>
 #include "ardualarm.h"
-
-// State enumeration - allows us to hold the entirety of our state in an 8bit int.
-enum State {
-	OUT_R_LED	= 0x01,
-	OUT_G_LED	= 0x02,
-	OUT_BUZZE	= 0x04,
-	IN_SMOKE	= 0x08,
-	//			= 0x10,
-	IN_TEMP_H	= 0x20,
-	//
-	IN_HUMI_H	= 0x80
-};
+#include "sense.h"
 
 // Component Objects
 LiquidCrystal lcd( LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7 );
 
 // Variables
-int temp, humi, tol;	// Hold readings from DHT11
-int lcdOutput=0;		// State variable: Contains LCD Output Mode
-
-int maxHumi=50;			// Humidity Threshold
-int maxTemp=25;			// Temperature Threshold
-
+LCDMode lcd_mode = ALL;
+Sensors sensors;
 uint8_t state = 0;
 
-//
+// Setup
 void setup()
-{ 
+{
+  // Correctly set output pins.
   pinMode(FMQ,OUTPUT);
   pinMode(LED_RED,OUTPUT);
   pinMode(LED_GRE,OUTPUT);
+
+  // Set hard coded min and max range.
+  sensors.sensMinTemp = TEMP_MIN;
+  sensors.sensMaxTemp = TEMP_MAX;
+  sensors.sensMinHumi = HUMI_MIN;
+  sensors.sensMaxHumi = HUMI_MAX;
+
+  // Set user defined alarm range.
+  sensors.maxTemp = 
+  sensors.maxHumi = 
 
   #ifdef DEBUG
   Serial.begin(9600);
   #endif
 }
 
-//
+// Main Loop
 void loop()
 {
   keyHumidityAdjust();		// Check for Humidity Interaction
   keyTempAdjust();			// Check for Temperature Interaction
   keyLCDModeScan();			// CHeck for Reset Interaction
 
-  while(! getDHTValues( DHT_SENSOR ) );
+  // Read from sensors
+  int maxTries = 5;
+  while( (! getDHTValues( DHT_SENSOR )) || ( maxTries > 0) ) maxTries--;
+  getSmokeReading();
 
   // BEGIN DEBUG:
-  Serial.print("temp:");
-  Serial.println(temp);
-  Serial.print("humi:");
-  Serial.println(humi);
-  Serial.print("tol:");
-  Serial.println(tol);
+  DEBUG_SEND("temp:");
+  DEBUG_SEND(sensors.temp);
+  DEBUG_SEND("humi:");
+  DEBUG_SEND(sensors.humi);
+  DEBUG_SEND("tol:");
+  DEBUG_SEND(sensors.tol);
   // EOF DEBUG.
 
+  // Delay, and then update our state from our new sensor readings
   delay(100);
   updateState();
 
   // Check for current LCD Output Mode
-  if( LCD_OUTPUT_ALL == lcdOutput )
-  {
+  switch( lcd_mode ){
+    case TEMP:
+      lcd_OutputTemp();
+      break;
 
-  	lcd.begin(16,2);
-    lcd.setCursor(0,0);
-    lcd.print("TEMP:  C");
-    lcd.setCursor(9,0);
-    lcd.print("SMO:");
-    lcd.setCursor(0,1);
-    lcd.print("HUMI:  %");
-    lcd.setCursor(9,1);
-    lcd.setCursor(5,0);
-    lcd.print(temp);  
-    lcd.setCursor(5,1);
-    lcd.print(humi);
-    lcd.setCursor(13,0);
-    lcd.print(val,DEC);
-  
-  } else if( LCD_OUTPUT_TEMP == lcdOutput ) {
+    case HUMI:
+      lcd_OutputHumi();
+      break;
 
-    lcd.begin(16,2);
-    lcd.print("ALARM TEMP: ");
-    lcd.setCursor(11,0);
-    lcd.print(maxTemp);
-
-  } else if( LCD_OUTPUT_HUMI == lcdOutput ) {
-
-    lcd.begin(16,2);
-    lcd.print("ALARM HUMI: ");	// Perhaps display the current val as well as the max val?
-    lcd.setCursor(12,0);
-    lcd.print(maxHumi);
-
+    case ALL:
+    default:
+      lcd_OutputAll();
+      break;
   }
 }
 
 
-// Detect BUTTON presses on Pin A3, and switch the LCD display 
-// output format.
-void keyLCDModeScan()
-{
-	if( buttonCheck( BTN_ADJUST_LCD ) )
-	{
-		lcdOutput++;
-		if( lcdOutput > 2 ) lcdOutput = 0;
-	}
-}
+/*
+** STATE MANIPULATION AND BEHAVIOUR
+*/
 
-// Detect BUTTON presses on Pin A1, and increase humidity threshold.
-//  Should humidity be set above 61, revert back to 20.
-//  Some form of UI feedback (LCD and DEBUG?) would be nice.
-void keyHumidityAdjust()
-{
-	if( buttonCheck( BTN_ADJUST_HUMI ) )
-	{
-		maxHumi++;
-		if( maxHumi > 60 ) maxHumi = 40;
-	}
-}
 
-// Detect BUTTON presses on Pin A2, and increase temperature 
-//  threshold. Should temperature be set above 31, revert back 
-//  to 20.
-//  Some form of UI feedback (LCD and DEBUG?) would be nice.
-void keyTempAdjust()
-{
-	if( buttonCheck( BTN_ADJUST_TEMP ) )
-	{
-		maxTemp++;
-		if( maxTemp > 30 ) maxTemp = 20;
-	}
-}
-
-// Check for a button press on a given PIN.
-bool buttonCheck( int pin )
-{
-	if( analogRead(pin) > 600 )
-	{
-		delay(20);
-		if( analogRead(pin) > 600 )
-		{
-			while( analogRead(pin) > 600 ); // Blocks execution until button is released.
-			return true;
-		}
-	}
-	return false;
-}
-
-// Get the values from the DHT; adheres to the protocol found in the 
-//  DHT11 Lib. ( http://playground.arduino.cc/main/DHT11Lib )
-bool getDHTValues( int pin )
-{
-	unsigned long time;		// Time
-	uint8_t bits[5];
-	uint8_t cnt = 7;
-	uint8_t idx = 0;
-	
-	delay(200);					// Delay
-	pinMode( pin,OUTPUT );		// Set DHT11 Signal (8) to output
-	digitalWrite( pin, LOW );		// Write low.
-	delay(20);					// Wait 20ms
-	digitalWrite( pin, HIGH );	// Write High
-	delayMicroseconds(40);		// delay 40ms
-	digitalWrite( pin, LOW );		// Write LOw
-	pinMode( pin,INPUT );			// Set to input
-
-	if(! checkForDHTAcknowledgement( pin, LOW ) )	return false;
-	if(! checkForDHTAcknowledgement( pin, HIGH ) )	return false;
-
-	for(int i=0;i<40;i++)
-	{
-		if(! checkForDHTAcknowledgement( pin, LOW ) )	return false;
-
-		time = micros();
-		if(! checkForDHTAcknowledgement( pin, HIGH ) )	return false;
-
-		if( (micros() - time) > 40 ) bits[idx] != (1 << cnt );
-		if( cnt == 0 )
-		{
-			cnt = 7;
-			idx++;
-		} else {
-			cnt--;
-		}
-	}
-
-	humi = bits[0];
-	temp = bits[2];
-	tol = bits[4];
-
-	if( bits[0] + bits[2] != tol )
-	{
-		DEBUG_SEND("DHT11 REPORTS INVALID VALUES.");	// Provide visual feedback?
-	    return false;
-	}
-
-	return true;
-}
-
-// Check for acknowledgement from the DHT.
-inline bool checkForDHTAcknowledgement( int pin, uint8_t level )
-{
-	// Check for acknowledgement; otherwise timeout.
-	unsigned int loopCnt = 10000;
-	while( digitalRead(pin) == level )
-	{
-		if( loopCnt-- == 0 )
-		{
-			DEBUG_SEND("DHT11 TIMED OUT.");	// Provide visual feedback?
-			return false;
-		}
-	}
-}
-
+// Given an updated set of sensor values, perform bitwise operations on 
+//  our state mask to produce a new state variable. If this is different
+//  from our previous state, trigger an update to all outputs.
 void updateState( )
 {
-	uint8_t newState;
+  uint8_t newState;
 
-	// Set state variables for sensors
-	newState |= ( humi > maxHumi ) << 8;
-	newState |= ( temp > maxTemp ) << 6;
-	newState |= ( analogRead(0) )	<< 4;
+  // Set state variables for sensors
+  newState |= ( sensors.humi > sensors.maxHumi )  << 8;
+  newState |= ( sensors.temp > sensors.maxTemp )  << 6;
+  newState |= ( sensors.smoke )	<< 4;
 
-	// One of the First 4 bits has been activated;
-	//  meaning a sensor has detected a dangerous reading.
-	if( newState & 0x0F ){
-		bool toggleAll = false;
-		if( newState & IN_SMOKE ) toggleAll = true;
-		if( newState & IN_HUMI_H || toggleAll ) newState |= 1 << 1;
-		if( newState & IN_TEMP_H || toggleAll ) newState |= 1 << 2; 
-	}
+  // One of the First 4 bits has been activated;
+  //  meaning a sensor has detected a dangerous reading.
+  if( newState & 0x0F ){
+    bool toggleAll = false;
+    if( newState & IN_SMOKE ) toggleAll = true;
+    if( newState & IN_HUMI_H || toggleAll ) newState |= 1 << 1;
+    if( newState & IN_TEMP_H || toggleAll ) newState |= 1 << 2; 
+  }
 
-	// If our state is different than the last reading, then lets update
-	//  our global state and refresh our outputs.
-	if( newState != state ){
-		state = newState;
-		updateIndicators();
-	}
+  // If our state is different than the last reading, then lets update
+  //  our global state and refresh our outputs.
+  if( newState != state ){
+    state = newState;
+    updateIndicators();
+  }
 }
 
+// Update all our outputs (Buzzer, Red LED and Green LED) from our
+//  state bitmask.
 void updateIndicators()
 {
-	digitalWrite( LED_GRE, ( state & OUT_G_LED ) );
-	digitalWrite( LED_RED, ( state & OUT_R_LED ) );
-	digitalWrite( FMQ, (! OUT_BUZZE ) );
+  digitalWrite( LED_GRE, ( state & OUT_G_LED ) );
+  digitalWrite( LED_RED, ( state & OUT_R_LED ) );
+  // Buzzer output is peculiar; high = off, low = on.
+  digitalWrite( FMQ, (! state & OUT_BUZZE ) );
 }
 
+
+/*
+** INPUT DETECTION/PROCESSING METHODS
+*/
+
+
+// Detect button presses on LCD output mode adjustment button.
+void keyLCDModeScan()
+{
+  // Some typecasting voodoo here, beware.
+  int mode = (int)lcd_mode;
+
+  keyIncrementer(
+    BTN_ADJUST_LCD,
+    0,
+    lcd_mode_count,
+    currentMode
+  );
+
+  lcd_mode = (LCDMode)current_mode;
+}
+
+// Detect button presses on humdity adjustment button.
+void keyHumidityAdjust()
+{
+  keyIncrementer(
+    BTN_ADJUST_HUMI, 
+    sensors.sensMinHumi,
+    sensors.sensMaxHumi,
+    sensors.maxHumi
+  );
+}
+
+// Detect button presses on temperature adjustment button.
+void keyTempAdjust()
+{
+  keyIncrementer(
+    BTN_ADJUST_TEMP, 
+    sensors.sensMinTemp,
+    sensors.sensMaxTemp,
+    sensors.maxTemp
+  );
+}
